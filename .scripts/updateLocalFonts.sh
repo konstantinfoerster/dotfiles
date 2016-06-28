1407cdefa2719c76adbc3f27ed1621fd7b6bdb2f
kgeorgiew <k.georgiew@gmail.com>
2016-06-28 10:40:12 +0200

    moved stlarch font into subdir, update font update script

--- .scripts/updateLocalFonts.sh
+++ .scripts/updateLocalFonts.sh
@@ -1,11 +1,18 @@
 #!/bin/sh
 
 FONT_DIR="$HOME/.local/share/fonts"
+
 if [ -d  "$FONT_DIR" ]; then 
-    cd "$FONT_DIR";
-    echo "Update fonts in $PWD";
-    xset +fp "$PWD" ;
-    mkfontscale;
-    mkfontdir;
-    xset fp rehash
-fi; 
+    cd "$FONT_DIR"
+    for dir in *; do
+      if [ -d $dir ]; then
+	cd "$dir"
+	echo "Update fonts in $PWD"
+	xset +fp "$PWD"
+	mkfontscale
+	mkfontdir
+	cd ..
+      fi
+    done && xset fp rehash
+fi 
+unset FONT_DIR
