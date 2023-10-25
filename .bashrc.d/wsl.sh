4639bc13321368593d1336a8d605051c0077fdf3
k.georgiew@gmail.com <konstantin.foerster@easy-software.com>
2023-10-25 08:44:15 +0200

    display is now set by wsl, alacritty with tmux

--- .bashrc.d/wsl.sh
+++ .bashrc.d/wsl.sh
@@ -1,13 +1,7 @@
 #!/bin/sh
 
 if grep -iq microsoft /proc/version ; then
-    display_target=$(grep nameserver /etc/resolv.conf | awk '{print $2; exit;}'):0
-    export DISPLAY=$display_target
-    export LIBGL_ALWAYS_INDIRECT=1
     export NO_AT_BRIDGE=1
     export VAGRANT_WSL_ENABLE_WINDOWS_ACCESS="1"
-    unset display_target
-
-    setxkbmap  de
 fi
 
