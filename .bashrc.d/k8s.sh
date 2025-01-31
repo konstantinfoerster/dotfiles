c9be441477572f8c66214ca8a426c28adaa14df7
k.georgiew@gmail.com <k.georgiew@gmail.com>
2025-01-31 08:28:12 +0100

    remove markdown since there is no real usage for now

--- .bashrc.d/k8s.sh
+++ .bashrc.d/k8s.sh
@@ -1,7 +1,9 @@
 alias k=kubectl
-alias kwRestarts="watch \"kubectl get po | awk '\\\$4>0'\""
 alias kge="kubectl get events --sort-by=.metadata.creationTimestamp"
 alias kgp="kubectl get pods --sort-by=.metadata.creationTimestamp"
+alias kl1m='kubectl logs --since 1m'
+alias klf1m='kubectl logs --since 1m -f'
 alias kc="k config current-context"
+
 source <(kubectl completion bash)
 complete -F __start_kubectl k
