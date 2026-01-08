fecce011dc99990d5e43b7406c17915b324aeda0
Konstantin Förster <k.georgiew@gmail.com>
2026-01-08 21:08:50 +0100

    fix some script lint warnings, remove obsolete files, update nvim plugins, install only latest jdk (jdk 25)

--- .scripts/gitClean.sh
+++ .scripts/gitClean.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 
-if [ "$1" == "-y" ]; then
-    git fetch -p && for branch in $(git branch -vv | grep ': gone]' | awk '{print $1}'); do git branch -d $branch; done
+if [ "$1" = "-y" ]; then
+    git fetch -p && for branch in $(git branch -vv | grep ': gone]' | awk '{print $1}'); do git branch -d "$branch"; done
 else 
     echo "Following branches would be deleted. Run with -y to delete these branches."
     git fetch -p && git branch -vv | grep ': gone]'
 fi
-
-
--- .scripts/gitPrune.sh
+++ .scripts/gitPrune.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 dryRun="--dry-run"
 
-if [ "$1" == "-y" ]; then
+if [ "$1" = "-y" ]; then
     dryRun=""
 else
     echo "running in dry mode. Add '-y' to disable dry-run"
