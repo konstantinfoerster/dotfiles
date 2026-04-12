07dfdf12b45a25716e0600aaa89f91ccda8b33f8
Konstantin Förster <k.georgiew@gmail.com>
2026-04-12 19:51:11 +0200

    update to latest treesitter version with compatible telescope version

--- .config/nvim/lua/kfoerster/plugins/treesitter.lua
+++ .config/nvim/lua/kfoerster/plugins/treesitter.lua
@@ -1,61 +1,49 @@
 return {
   "nvim-treesitter/nvim-treesitter",
-  branch = "master",
+  branch = "main",
+  lazy = false,
   build = ":TSUpdate",
   config = function()
-    require("nvim-treesitter.configs").setup({
-      ensure_installed = {
-        "c",
-        "bash",
-        "css",
-        "dockerfile",
-        "diff",
-        "go",
-        "gomod",
-        "gosum",
-        "gotmpl",
-        "groovy",
-        "html",
-        "java",
-        "javascript",
-        "json",
-        "lua",
-        "markdown",
-        "markdown_inline",
-        "make",
-        "python",
-        "toml",
-        "typescript",
-        "vim",
-        "vimdoc",
-        "vue",
-        "query",
-        "xml",
-        "yaml",
-      },
-      ignore_install = { "properties" }, -- cause errors in healthcheck
+    local parsers = {
+      "c",
+      "bash",
+      "css",
+      "dockerfile",
+      "diff",
+      "go",
+      "gomod",
+      "gosum",
+      "gotmpl",
+      "groovy",
+      "html",
+      "java",
+      "javascript",
+      "json",
+      "lua",
+      "markdown",
+      "markdown_inline",
+      "make",
+      "python",
+      "toml",
+      "typescript",
+      "vim",
+      "vimdoc",
+      "vue",
+      "query",
+      "xml",
+      "yaml",
+    }
+    require("nvim-treesitter").install(parsers)
 
-      -- Install parsers synchronously (only applied to `ensure_installed`)
-      sync_install = false,
+    -- enable highlighting
+    vim.api.nvim_create_autocmd("FileType", {
+      callback = function()
+        -- Enable treesitter highlighting and disable regex syntax
+        pcall(vim.treesitter.start)
 
-      -- Automatically install missing parsers when entering buffer
-      -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-      auto_install = false,
-
-      indent = {
-        enable = true,
-      },
-
-      highlight = {
-        -- `false` will disable the whole extension
-        enable = true,
-
-        -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-        -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-        -- Using this option may slow down your editor, and you may see some duplicate highlights.
-        -- Instead of true it can also be a list of languages
-        additional_vim_regex_highlighting = false,
-      },
+        -- Enable treesitter-based indentation
+        vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+      end,
     })
   end,
 }
